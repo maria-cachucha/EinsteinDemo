76f5f0a6b40f8480a07fd29cd2da6a61eded2806
Maria Cachucha <maria.cachucha@outlook.com>
2019-11-06 07:58:47 +0000

    First Commit

---
+++ HelloWorld.py
@@ -0,0 +1,7 @@
+#
+#  File: 'HelloWorld.py'    Author: Maria Cachucha      Date:06-11-2019
+#
+#   Description: This file prints to screen the words "Hello World" using python 3.6
+#
+
+print("Hello World!")
